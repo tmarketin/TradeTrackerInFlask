7d424a10c52b6455635c98bb32c41ecb1ef89817
Tomislav Marketin <tomislav.marketin@gmail.com>
2020-09-15 23:28:32 +0200

    Introducing templates into app

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,29 @@
 from app import appInstance
+from flask import render_template
 
 @appInstance.route('/')
 @appInstance.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Tomislav'}
+    people = [{
+        'name': 'Edward Teller',
+        'field': 'Nuclear Physics',
+        'country': 'Hungary'
+    },
+    {
+        'name': 'Stanislaw Ulam',
+        'field': 'Mathematics',
+        'country': 'Austria-Hungary'
+    }
+    ]
+    return render_template('index.html', title="Home", people=people)
+
+@appInstance.route('/jsontest')
+def jsonTest():
+    people = {
+        'name': 'Edward Teller',
+        'field': 'Nuclear Physics',
+        'country': 'Hungary'
+    }
+
+    return people

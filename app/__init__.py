620ca97de2a0f6bbf364fa6ee2653629e0240c58
Tomislav Marketin <tomislav.marketin@gmail.com>
2020-09-16 12:56:07 +0200

    Chapter 3, user login and input validation

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 appInstance = Flask(__name__)
+appInstance.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import appInstance
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @appInstance.route('/')
 @appInstance.route('/index')
@@ -17,6 +18,17 @@ def index():
     }
     ]
     return render_template('index.html', title="Home", people=people)
+
+@appInstance.route('/login', methods = ['GET', 'POST'])
+def login():
+    formInstance  = LoginForm()
+    if formInstance.validate_on_submit():
+        flash('Login requested for user {}, rememberMe = {}'.format(
+            formInstance.username.data, formInstance.rememberMe.data)
+            )
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title = 'Sign In', form = formInstance)
 
 @appInstance.route('/jsontest')
 def jsonTest():

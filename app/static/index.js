85153b5f12d1c46adce06a0ea7a84581c69b19e0
Tomislav Marketin <tomislav.marketin@gmail.com>
2020-11-24 22:17:54 +0100

    New filtering, implemented all filters

--- app/static/index.js
+++ app/static/index.js
@@ -120,10 +120,14 @@ function resetFilters() {
     for(let id of idList) {
         document.getElementById(id).getElementsByTagName("span")[0].innerHTML = "All";
     }
+    filterContent();
 }
 
 function filterContent() {
     let idStatus = "btn-filter-status";
+    let idStrategy = "btn-filter-strategy";
+    let idTicker = "btn-filter-ticker";
+    let idYear = "btn-filter-year";
 
     let filterStatusValue = document.getElementById(idStatus).getElementsByTagName("span")[0].innerHTML;
     let filterStatusClassMap = new Map();
@@ -132,9 +136,22 @@ function filterContent() {
     filterStatusClassMap.set('Wins', 'panel-success');
     filterStatusClassMap.set('Losses', 'panel-danger');
 
+    let filterStrategyValue = document.getElementById(idStrategy).getElementsByTagName("span")[0].innerHTML;
+    let filterTickerValue = document.getElementById(idTicker).getElementsByTagName("span")[0].innerHTML;
+    let filterYearValue = document.getElementById(idYear).getElementsByTagName("span")[0].innerHTML;
+
     let tradesList = document.getElementsByClassName("panel");
     for(let idx = 0; idx < tradesList.length; idx++) {
-        if (tradesList[idx].classList.contains(filterStatusClassMap.get(filterStatusValue))) {
+        let isStatusFilterOk = tradesList[idx].classList.contains(filterStatusClassMap.get(filterStatusValue));
+        let isStrategyFilterOk = (tradesList[idx].getElementsByClassName("panel-heading-strategy")[0].innerHTML == filterStrategyValue)
+                                || (filterStrategyValue == "All");
+        let isTickerFilterOk = (tradesList[idx].getElementsByClassName("panel-heading-ticker")[0].innerHTML == filterTickerValue)
+                                || (filterTickerValue == "All");
+
+        let oDate = new Date(tradesList[idx].getElementsByClassName("panel-body-open-date")[0].innerHTML);
+        let isYearFilterOk = (oDate.getFullYear() == filterYearValue) || (filterYearValue == "All");
+        
+        if (isStatusFilterOk && isStrategyFilterOk && isTickerFilterOk && isYearFilterOk) {
             tradesList[idx].style.display = "block";
         }
         else {
